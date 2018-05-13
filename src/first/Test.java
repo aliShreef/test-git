a9e08994c891de75e27ec96476135e4627c642fa
alishreef <alishreef30@gmail.com>
2018-05-13 10:09:32 +0300

    add date display

--- src/first/Test.java
+++ src/first/Test.java
@@ -1,10 +1,15 @@
 package first;
 
+import java.util.Date;
+
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
 		System.out.println("Test2");
+		System.out.println("Test2");
+		System.out.println(new Date());
+		
 		
 	}
 }
